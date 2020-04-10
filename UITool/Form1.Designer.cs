1ec1bfa326ede291869d50555d0d68a9572dc4c6
bobaUla <>
2020-04-11 01:04:06 +0200

    Implemted simple visualization

--- UITool/Form1.Designer.cs
+++ UITool/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCSVPath = new System.Windows.Forms.Button();
+            this.btnReadHead = new System.Windows.Forms.Button();
+            this.btnReadTable = new System.Windows.Forms.Button();
+            this.btnReadBody = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.tbCSVPath = new System.Windows.Forms.TextBox();
+            this.tbTableLine = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCSVPath
+            // 
+            this.btnCSVPath.Location = new System.Drawing.Point(12, 12);
+            this.btnCSVPath.Name = "btnCSVPath";
+            this.btnCSVPath.Size = new System.Drawing.Size(115, 23);
+            this.btnCSVPath.TabIndex = 0;
+            this.btnCSVPath.Text = "Get CSV-Path";
+            this.btnCSVPath.UseVisualStyleBackColor = true;
+            this.btnCSVPath.Click += new System.EventHandler(this.btnCSVPath_Click);
+            // 
+            // btnReadHead
+            // 
+            this.btnReadHead.Location = new System.Drawing.Point(12, 112);
+            this.btnReadHead.Name = "btnReadHead";
+            this.btnReadHead.Size = new System.Drawing.Size(115, 23);
+            this.btnReadHead.TabIndex = 1;
+            this.btnReadHead.Text = "ReadHead";
+            this.btnReadHead.UseVisualStyleBackColor = true;
+            this.btnReadHead.Click += new System.EventHandler(this.btnReadHead_Click);
+            // 
+            // btnReadTable
+            // 
+            this.btnReadTable.Location = new System.Drawing.Point(12, 141);
+            this.btnReadTable.Name = "btnReadTable";
+            this.btnReadTable.Size = new System.Drawing.Size(115, 23);
+            this.btnReadTable.TabIndex = 2;
+            this.btnReadTable.Text = "ReadTable";
+            this.btnReadTable.UseVisualStyleBackColor = true;
+            this.btnReadTable.Click += new System.EventHandler(this.btnReadTable_Click);
+            // 
+            // btnReadBody
+            // 
+            this.btnReadBody.Location = new System.Drawing.Point(12, 170);
+            this.btnReadBody.Name = "btnReadBody";
+            this.btnReadBody.Size = new System.Drawing.Size(115, 23);
+            this.btnReadBody.TabIndex = 3;
+            this.btnReadBody.Text = "ReadBody";
+            this.btnReadBody.UseVisualStyleBackColor = true;
+            this.btnReadBody.Click += new System.EventHandler(this.btnReadBody_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(133, 41);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(655, 397);
+            this.rtbOutput.TabIndex = 4;
+            this.rtbOutput.Text = "";
+            // 
+            // tbCSVPath
+            // 
+            this.tbCSVPath.Location = new System.Drawing.Point(133, 12);
+            this.tbCSVPath.Name = "tbCSVPath";
+            this.tbCSVPath.Size = new System.Drawing.Size(655, 22);
+            this.tbCSVPath.TabIndex = 5;
+            // 
+            // tbTableLine
+            // 
+            this.tbTableLine.Location = new System.Drawing.Point(73, 41);
+            this.tbTableLine.Name = "tbTableLine";
+            this.tbTableLine.Size = new System.Drawing.Size(54, 22);
+            this.tbTableLine.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbTableLine);
+            this.Controls.Add(this.tbCSVPath);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnReadBody);
+            this.Controls.Add(this.btnReadTable);
+            this.Controls.Add(this.btnReadHead);
+            this.Controls.Add(this.btnCSVPath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCSVPath;
+        private System.Windows.Forms.Button btnReadHead;
+        private System.Windows.Forms.Button btnReadTable;
+        private System.Windows.Forms.Button btnReadBody;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox tbCSVPath;
+        private System.Windows.Forms.TextBox tbTableLine;
     }
 }
 
--- UITool/Form1.cs
+++ UITool/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSV2DBConverter;
+using CSV2DBConverter.Adapter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,65 @@ namespace UITool
 {
     public partial class Form1 : Form
     {
+        CSVReader _CSVReader;
+        IFileReader _fileReader;
         public Form1()
         {
             InitializeComponent();
+            _fileReader = new FileReader();
+            _CSVReader = new CSVReader(tbCSVPath.Text, _fileReader);
         }
+
+        private void btnCSVPath_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK && fileDialog.FileNames.Length == 1)
+            {
+                tbCSVPath.Text = fileDialog.FileName;
+            }
+        }
+
+        private void btnReadHead_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbCSVPath.Text))
+                return;
+
+            rtbOutput.Clear();
+            var csvReader = new CSVReader(tbCSVPath.Text, _fileReader);
+            csvReader.Analyze(int.Parse(tbTableLine.Text));
+            PrintLines(csvReader.Head);
+        }
+
+        private void btnReadTable_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbCSVPath.Text))
+                return;
+
+            rtbOutput.Clear();
+            var csvReader = new CSVReader(tbCSVPath.Text, _fileReader);
+            csvReader.Analyze(int.Parse(tbTableLine.Text));
+            PrintLines(csvReader.Table);
+        }
+
+        private void btnReadBody_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbCSVPath.Text))
+                return;
+
+            rtbOutput.Clear();
+            var csvReader = new CSVReader(tbCSVPath.Text, _fileReader);
+            csvReader.Analyze(int.Parse(tbTableLine.Text));
+            PrintLines(csvReader.Body);
+        }
+
+        private void PrintLines(string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                rtbOutput.AppendText(line + "\n");
+            }
+        }
+
     }
 }
