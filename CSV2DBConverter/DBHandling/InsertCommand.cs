b8c54b68bf897c586f55238fbadca78e9b060aa5
bobaUla <>
2020-04-11 20:17:22 +0200

    added insert command

---
+++ CSV2DBConverter/DBHandling/InsertCommand.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.SQLite;
+
+namespace CSV2DBConverter.DBHandling
+{
+    public interface IInsertCommand
+    {
+        void Insert();
+    }
+
+    public class InsertCommand : IInsertCommand
+    {
+        private readonly IDBConnectionString connectionString;
+        private readonly string tableName;
+        private readonly CSVRow csvEntries;
+
+        public InsertCommand(
+            IDBConnectionString connectionString, 
+            string tableName, 
+            CSVRow csvEntries)
+        {
+            this.connectionString = connectionString;
+            this.tableName = tableName;
+            this.csvEntries = csvEntries;
+        }
+
+        public void Insert()
+        {
+            using (var con = new SQLiteConnection(connectionString.ConnectionString))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand(con);
+                
+                cmd.CommandText = CreateInsertCommand();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private string CreateInsertCommand()
+        {
+            var command = $"Insert into {tableName}(";
+            var entries = csvEntries.Row;
+            var entriesWithoutLastOne = csvEntries.Row.Count - 1;
+            for (var entry = 0; entry < entriesWithoutLastOne; entry++)
+            {
+                command += entries[entry].Key + ",";
+            }
+            command += entries[entriesWithoutLastOne].Key + ")";
+
+            command += " Values(";
+            for (var entry = 0; entry < entriesWithoutLastOne; entry++)
+            {
+                command += "'" + entries[entry].Value + "'" + ",";
+            }
+            command += "'" + entries[entriesWithoutLastOne].Value + "'";
+            command += ");";
+            return command;
+        }
+    }
+
+}
